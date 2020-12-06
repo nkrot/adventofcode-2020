3667034dba9a0445a733283618f2bb9fa832794e
Nikolai Krot <talpus@gmail.com>
2020-12-06 12:11:11 +0100

    add docstrings

--- aoc/utils.py
+++ aoc/utils.py
@@ -1,10 +1,31 @@
 
-from typing import List
+from typing import List, Union
 
 
 def load_input(fname: str = 'input.txt') -> List[str]:
+    """Load file, either given or default 'input.txt' and return its content
+    as a list of lines. All lines are returned, including empty ones."""
     lines = []
     with open(fname) as fd:
         for line in fd:
             lines.append(line.strip())
     return lines
+
+
+
+def group_lines(data: Union[str,List[str]]) -> List[List[str]]:
+    """Make groups of lines: a group is a sequence of lines that are separated
+    by an empty line from another group.
+    Input <data> is either of these:
+    1) (str) a string that is the whole content of a file;
+    2) (List[str]) a list of lines that 1) already split into individual lines.
+    """
+    groups = [[]]
+    if isinstance(data, str):
+        data = [ln.strip() for ln in data.split('\n')]
+    for ln in data:
+        if ln:
+            groups[-1].append(ln)
+        else:
+            groups.append([])
+    return groups
