851949f03687181405140c3d2c4aae2fcb3badb9
Nikolai Krot <n.krot@drooms.com>
2020-12-10 20:42:38 +0100

    solve day 10 p.2

--- day_10/solution.py
+++ day_10/solution.py
@@ -31,10 +31,52 @@ def solve_p1(numbers: List[int]) -> int:
     return counts[1] * counts[3]
 
 
-def solve_p2(data):
+def solve_p2(numbers: List[int]) -> int:
     """Solution to the 2nd part of the challenge"""
-    # TODO
-    pass
+    nums = sorted(numbers)
+    if nums[0] > 0:
+        nums.insert(0, 0)
+    nums.append(nums[-1]+3)
+    if DEBUG:
+        print(nums)
+    mx = nums[-1]
+    mat = [[0] * (1+mx) for i in range(mx+1)]
+
+    # connect adapters according to rules
+    for r, nr in enumerate(nums):
+        if r == 0:
+            continue
+        for c in {max(r-3, 0), max(r-2, 0), max(r-1, 0)}:
+            nc = nums[c]
+            diff = nr - nc
+            assert diff != 0, "Shit happened"
+            if diff < 4:
+                # print(nc, nr, diff)
+                mat[nr][nc] += 1
+
+    # update the number of previous adapters
+    for ri, row in enumerate(mat):
+        n_ins = sum(row)
+        if DEBUG:
+            print(n_ins, ri, row)
+        if n_ins > 1:
+            # check next 3 rows
+            for rj in range(ri+1, ri+4):
+                if rj < len(mat) and mat[rj][ri] > 0:
+                    mat[rj][ri] += (n_ins-1)
+
+    if DEBUG:
+        show(mat)
+
+    return sum(mat[-1])
+
+
+def show(m):
+    lines = []
+    for ri, r in enumerate(m):
+        row = [f'{n:>2}' for n in r]
+        lines.append(f"{ri:<3}:" + ", ".join(row))
+    print("\n".join(lines))
 
 
 text1 = """16
@@ -51,8 +93,8 @@ text1 = """16
 
 
 tests = [
-    (text1.split('\n'), 7*5, -1),
-    (utils.load_input('test_input.1.txt'), 22*10, -1)
+    (text1.split('\n'), 7*5, 8),
+    (utils.load_input('test_input.1.txt'), 22*10, 19208)
 ]
 
 
@@ -65,8 +107,8 @@ def run_tests():
         res1 = solve_p1(inp)
         print(f"T1.{tid}:", res1 == exp1, exp1, res1)
 
-        # res2 = solve_p2(inp)
-        # print(f"T2.{tid}:", res2 == exp2, exp2, res2)
+        res2 = solve_p2(inp)
+        print(f"T2.{tid}:", res2 == exp2, exp2, res2)
 
 
 def run_real():
@@ -78,10 +120,10 @@ def run_real():
     res1 = solve_p1(lines)
     print(exp1 == res1, exp1, res1)
 
-    # print(f"--- Day {day} p.2 ---")
-    # exp2 = 360
-    # res2 = solve_p2(lines)
-    # print(exp2 == res2, exp2, res2)
+    print(f"--- Day {day} p.2 ---")
+    exp2 = 12401793332096
+    res2 = solve_p2(lines)
+    print(exp2 == res2, exp2, res2)
 
 
 if __name__ == '__main__':
